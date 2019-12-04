0e0cc5cba5b17bec981c7a0258cd6285dad6a040
rahulkotha18 <rahul.kotha@zemosolabs.com>
2019-12-04 12:37:01 +0530

    use Hooks

--- gre-ui/src/App.js
+++ gre-ui/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 import Login from "./Login";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Login />
-      </div>
-    );
-  }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Login />
+//       </div>
+//     );
+//   }
+// }
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
-
 export default App;
